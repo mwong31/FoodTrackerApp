2b6cf7883ccbff0a8ef5e233db453e061221450f
Marisa Wong <marisawong@berkeley.edu>
2016-10-02 03:21:33 -0700

    Uploading FoodTracker

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,35 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Marisa Wong on 10/2/16.
+//  Copyright © 2016 Marisa Wong. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
